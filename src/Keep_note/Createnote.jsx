59e428a8742360b65cab810c53fff483f8bf360f
Anil <nitanilkumar.com>
2022-01-21 03:47:08 +0530

    updated code

--- src/Keep_note/Createnote.jsx
+++ src/Keep_note/Createnote.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Createnote = (props) => {
+    const [expend, setexpend]=useState(false);
+
     const [note, setNote] = useState({
         title: '',
         content: ''
@@ -28,13 +30,23 @@ const Createnote = (props) => {
 
     };
 
+    const expendIt=()=>{
+        setexpend(true);
+    };
+    const backtoNormal=()=>{
+        setexpend(false);
+    };
+
 
     return (<>
-        <div className='main_div'>
+        <div className='main_div' onDoubleClick={backtoNormal}>
             <h1>Notes</h1>
-            <input type='text' name='title' value={note.title} onChange={inputEvent} placeholder='Enter the tile' autoComplete='off' /><br />
-            <textarea rows='' column='' name='content' value={note.content} onChange={inputEvent} placeholder='Enter Text'></textarea><br />
-            <button onClick={addEvent}>Add Note</button>
+            {expend?
+            <input type='text' name='title' value={note.title} onChange={inputEvent} placeholder='Enter the tile' autoComplete='off' />
+            :null}
+            <br />
+            <textarea rows='' column='' name='content' value={note.content} onChange={inputEvent} placeholder='Enter Text' onClick={expendIt}></textarea><br />
+            {expend?<button onClick={addEvent}>Add Note</button>:null}
 
         </div>
 
